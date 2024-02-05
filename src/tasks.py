348204bb29e58e3b421d548a953b0d2c46d78258
dignjatovic <dusan@quantlabs.rs>
2024-02-05 06:38:40 +0100

    fix issue

--- src/tasks.py
+++ src/tasks.py
@@ -6,5 +6,5 @@ from src.countries.ingest.etl_builder import CountryETLFactory
 @app.task
 def ingest_countries():
     with SessionLocal() as db:
-        etl = CountryETLFactory(db).set_fetcher().set_processor().set_loader().create()
+        etl = CountryETLFactory(db).create()
         etl.run_pipeline()
